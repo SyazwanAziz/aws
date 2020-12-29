163113dc36312c22f1ea8a22d614c39fe4ae2c70
SyazwanAziz <57909182+SyazwanAziz@users.noreply.github.com>
2020-12-29 16:52:17 +0800

    first commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:fb758de5-168f-438a-a00b-d7fe5904156b",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_a2PRtl0o9",
+                        "AppClientId": "62rgllb4vrqjavfgveisueufu4",
+                        "AppClientSecret": "s92s8slsrt3epp0g6ailkjjja9rajv07bvbp2bk8rkj02q08vo",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
